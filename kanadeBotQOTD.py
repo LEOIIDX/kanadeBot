918d200d6ac29b39098c622e6ca6c1fbde977adc
leo <chathoo34@gmail.com>
2021-08-09 04:08:27 -0500

    building QOTD Hijack

--- kanadeBotQOTD.py
+++ kanadeBotQOTD.py
@@ -21,7 +21,7 @@ from leoDictionary import dictionaryMessages, dictionaryStatuses
 from leoEmbed import iidxSPClassEmbeds, iidxDPClassEmbeds, sdvxDanEmbeds, infoEmbeds
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DEBUG_TOKEN')
 
 #intents
 intents = discord.Intents.default()
@@ -31,34 +31,14 @@ intents.reactions = True
 
 print('Kanade Bot QOTD module\n')
 
-stDict = dictionaryStatuses()
-
-for key in stDict.multiStatus:
-	print(key + ' ' + stDict.multiStatus.get(key))
-
-'''
-command section
-command prefix ky!
-'''
 bot = commands.Bot(command_prefix='ky!',intents=intents)
-
-bot.remove_command('help')
 
 qotdCh = 867088318466359338
 
 @bot.event
 async def on_ready():
-	sDict = dictionaryStatuses()
-	qDict = dictionaryMessages()
-	uQotd = {}
-	uQotdCount = 0
-
-	readyVer = 1
-	sPick = str(input('Input Desired Status. \n (0) for random \n'))
-
-	await bot.change_presence(status=discord.Status.online, activity=discord.Game(sDict.multiStatus.get(sPick)))
-
-	if debugValue == 0:
-		print('Bot is ready!!\n')
+    print('Sending QOTD')
+    await bot.get_channel(qotdCh).send('QOTD Bot Hijack test')
+    exit()
 
 bot.run(TOKEN)
