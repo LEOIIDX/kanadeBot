34ec6b57ad11e282dbe18d4038e8144a117544b1
leo <chathoo34@gmail.com>
2022-02-13 02:39:06 -0600

    About finished the frame work for the new type of qotd

--- kanadeBotDBQOTD.py
+++ kanadeBotDBQOTD.py
@@ -13,6 +13,7 @@ import re
 import asyncio
 import string
 import math
+import json
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -26,13 +27,46 @@ intents.members = True
 intents.emojis = True
 intents.reactions = True
 
-print('Kanade Bot COTM module\n')
+print('Kanade Bot DEATH BATTLE COTD module\n')
 
 bot = commands.Bot(command_prefix='kyt!',intents=intents)
 
-cotwCh = 922405898662600755
+qotdTestCh = 867088318466359338
+
+async def dataPrep():
+	global charaList, imgList, imgList_COUNT
+
+	imgList_COUNT = 0
+
+	with open ('DB.json', 'r', encoding='utf8') as f:
+		charaList = json.load(f)
+
+	imgList = os.listdir("dbIMG/")
+
+	for item in imgList:
+		imgList_COUNT = imgList_COUNT + 1
+
+async def embedCreator():
+	ran = (random.randint(1, imgList_COUNT) - 1)
+
+	charaEmbed = discord.Embed()
+
+	file = discord.File('dbIMG/'+ str(ran)  +'.png', filename= str(ran) + '.png')
+
+	charaEmbed.set_image(url='attachment://'+ str(ran) +'.png')
+	charaEmbed.add_field(name=charaList[ran]['name'], value=charaList[ran]['source'])
+	charaEmbed.set_footer(text='Nanahira Monke Vs.')
+
+	await bot.get_channel(qotdTestCh).send(file=file, embed=charaEmbed)
 
 @bot.event
 async def on_ready():
+	await dataPrep()
+
+	file = discord.File('dbIMG/' + str(charaList[0]['id']) + '.png')
+
+	await embedCreator()
+	await bot.get_channel(qotdTestCh).send('\nVs.\n')
+	await embedCreator()
 	exit()
 bot.run(TOKEN)
