8a85bfffc6233e4cf55d0bd923a5b62ddfd91b67
leo <chathoo34@gmail.com>
2022-05-25 04:39:57 -0500

    Got to the point where the bot runs and responds to messages.

---
+++ main.js
@@ -0,0 +1,32 @@
+/* main.js
+ *
+ * Kanade Bot in JavaScript
+ * Nanahira Monke Kanade Dev
+ *
+ * */
+
+console.log("Kanade Bot");
+
+const { Client, Intents } = require('discord.js');i /*!important/import for discord.js*/
+
+require('dotenv').config(); /* Loads the .env file in working directoryi */
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
+
+/* Runs when the bot is ready, Sets status. */
+client.once('ready', () => {
+	console.log('Ready');
+	if (process.env.DEBUG_VALUE != 0) client.user.setActivity("KANADE BOT JS");
+	else client.user.setActivity("placeholder");
+});
+
+/* Message response event handler (this will be interesting) */
+client.on("messageCreate", message => {
+	if (message.author.id == 840279273021636628) return;
+
+	message.channel.send('placeholder message');
+	return
+});
+
+/* Logs in the bot using a token from your local .env */
+client.login(process.env.DISCORD_TOKEN);
